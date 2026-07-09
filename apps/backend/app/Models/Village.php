062a6618ea10d19a1db9593455615958c4fa8ec7
Revi Permana <revipermana3@gmail.com>
2026-07-09 14:20:52 +0700

    feat: tambah bmodel village + hasMany

---
+++ apps/backend/app/Models/Village.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Village extends Model
+{
+    protected $fillable = [
+        'name',
+        'head_name',
+        'address',
+        'history',
+        'vision',
+        'mission',
+        'wa_contact',
+    ];
+
+    public function hamlets()
+    {
+        return $this->hasMany(Hamlet::class);
+    }
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+    public function citizen()
+    {
+        return $this->hasMany(Citizen::class);
+    }
+    public function news()
+    {
+        return $this->hasMany(News::class);
+    }
+    public function letters()
+    {
+        return $this->hasMany(Letter::class);
+    }
+    public function VillageRegulations()
+    {
+        return $this->hasMany(VillageRegulation::class);
+    }
+}
