43d41ff5e84c3392d5bc68bc4fbe13efd84816ed
ryhndastra <ryhndastra@gmail.com>
2026-07-10 14:10:09 +0700

    fix(login): strenghten password validation

--- apps/frontend/src/features/auth/components/LoginForm.jsx
+++ apps/frontend/src/features/auth/components/LoginForm.jsx
@@ -1,50 +1,21 @@
-import { useState } from "react";
+import { useLoginForm } from "@/features/auth/hooks/useLoginForm";
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 
 export function LoginForm() {
-  const [formData, setFormData] = useState({ nik: "", password: "" });
-  const [errors, setErrors] = useState({});
+  // call state dan logic dari custom hook
+  const { formData, errors, handleChange, handleSubmit } = useLoginForm();
 
-  const validate = () => {
-    const newErrors = {};
-
-    if (!formData.nik) {
-      newErrors.nik = "NIK wajib diisi";
-    } else if (!/^\d+$/.test(formData.nik)) {
-      newErrors.nik = "NIK hanya boleh berisi angka";
-    } else if (formData.nik.length !== 16) {
-      newErrors.nik = "NIK harus 16 digit";
-    }
-
-    if (!formData.password) {
-      newErrors.password = "Password wajib diisi";
-    }
-
-    setErrors(newErrors);
-    return Object.keys(newErrors).length === 0;
-  };
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-
-    if (name === "nik" && value !== "" && !/^\d+$/.test(value)) {
-      return;
-    }
-
-    setFormData({ ...formData, [name]: value });
-    if (errors[name]) setErrors({ ...errors, [name]: null });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (validate()) {
-      console.log("Validasi FE Sukses! Data siap kirim ke API:", formData);
-    }
+  const handleLoginSuccess = (data) => {
+    console.log("Validasi FE Sukses! Data siap kirim ke API:", data);
+    // integrasi api ticket code SID-27 tar disini
   };
 
   return (
-    <form onSubmit={handleSubmit} className="w-full">
+    <form
+      onSubmit={(e) => handleSubmit(e, handleLoginSuccess)}
+      className="w-full"
+    >
       <Input
         label="NIK"
         name="nik"
