9f3f5315f65d5273d4b4588295b159f982276602
aaz132 <68881352+aaz132@users.noreply.github.com>
2026-07-13 21:36:41 +0700

    feat(user,citizen,officials): penyesuaian model dan migrasi agar sesuai dengan erd (#24)
    * feat: model user + officials + citizens with relasi
    
    * Merge branch 'dev' of https://github.com/Vextronode/sid-app into feature/model-user-relasi
    
    * squash merge from hamlets/rt/rw
    
    * feat(user,citizen,officials): penyesuaian model dan migrasi agar sesuai dengan erd

--- apps/backend/database/migrations/2026_07_10_074713_create_citizens_table.php
+++ apps/backend/database/migrations/2026_07_10_074713_create_citizens_table.php
@@ -13,13 +13,24 @@ return new class extends Migration
     {
         Schema::create('citizens', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('rt_id')->constrained('rts')->onDelete('cascade');
-            $table->foreignId('hamlet_id')->constrained('hamlets')->onDelete('cascade');
-            $table->string('nik')->unique();
-            $table->string('name');
-            $table->string('religion');
-            $table->string('last_education');
-            $table->string('domicile_status');
+            $table->foreignId('village_id')->constrained()->cascadeOnDelete();
+            $table->text('nik');
+            $table->string('nik_hash')->unique();
+            $table->string('name', 100);
+            $table->date('date_of_birth');
+            $table->string('place_of_birth', 100)->nullable();
+            $table->enum('gender', ['L', 'P']);
+            $table->text('address');
+            $table->foreignId('rt_id')->nullable()->constrained('rts')->nullOnDelete();
+            $table->foreignId('hamlet_id')->nullable()->constrained()->nullOnDelete();
+            $table->text('no_kk', 16)->nullable();
+            $table->enum('marital_status', ['belum_kawin', 'kawin', 'cerai_hidup', 'cerai_mati'])->nullable();
+            $table->string('occupation', 100)->nullable();
+            $table->enum('religion', ['islam', 'kristen', 'katolik', 'hindu', 'budha', 'konghucu'])->nullable();
+            $table->enum('last_education', ['tidak_sekolah', 'sd', 'smp', 'sma', 'd3', 's1', 's2', 's3'])->nullable();
+            $table->enum('domicile_status', ['menetap', 'merantau_dalam_negeri', 'merantau_luar_negeri','tki'])->default('menetap');
+            $table->string('current_domicile', 150)->nullable();
+            $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
     }
