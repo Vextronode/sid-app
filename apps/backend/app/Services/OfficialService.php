ea89c2a4f3092602250aa4e3fa3ce5e8b46382b2
sahid124 <kerensyahid@gmail.com>
2026-07-16 00:52:50 +0700

    refactor: adjust official, citizen, rt, and rw relationships

--- apps/backend/app/Services/OfficialService.php
+++ apps/backend/app/Services/OfficialService.php
@@ -11,26 +11,36 @@ class OfficialService
 
     public function resolveRtForCitizen(Citizen $citizen)
     {
-        return Official::where(
-                'rt_id',
-                $citizen->rt_id
-            )
-            ->where(
-                'position',
-                'rt'
-            )
-            ->where(
-                'is_active',
-                true
-            )
+        return Official::where('rt_id', $citizen->rt_id)
+            ->where('position', 'rt')
+            ->where('is_active', true)
             ->first();
     }
+
 
     public function getCurrentOfficial(User $user): Official
     {
         return $user->official()
             ->where('is_active', true)
             ->firstOrFail();
+    }
+
+
+    public function getCurrentRw(User $user): Official
+    {
+        return $user->official()
+            ->where('position', 'rw')
+            ->where('is_active', true)
+        ->firstOrFail();
+    }
+
+
+    public function getCurrentRt(User $user): Official
+    {
+        return $user->official()
+            ->where('position', 'rt')
+            ->where('is_active', true)
+        ->firstOrFail();
     }
 
 }
