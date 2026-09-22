138c13d7199e4fbe34384590ca781b5791576d07
Hanchan <handikacp155@gmail.com>
2026-09-22 15:23:30 +0700

    fix typo in namespace RegisteredUserRequest

--- apps/backend/app/Http/Controllers/Auth/RegisteredUserController.php
+++ apps/backend/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\auth\RegisterUserRequest;
+use App\Http\Requests\Auth\RegisterUserRequest;
 use App\Services\Auth\AuthService;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Response;
